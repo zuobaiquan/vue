be1ad453272a6c1e80d6fea1ac4385bbe1870b1e
zuobaiquan <zuobaiquan@qq.com>
2018-04-02 11:57:32 +0800

    dds

---
+++ 我的vue练习项目/vue-test/src/views/bargraph/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    1111
+  </div>
+</template>
+<script>
+export default {
+  name: 'test',
+  data(){
+    return {
+
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+<style media="screen" lang="scss">
+
+</style>
