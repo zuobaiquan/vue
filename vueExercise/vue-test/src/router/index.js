9271e8f9250c180d1bf29f0be39f2b887afb4c8f
zuobaiquan <zuobaiquan@qq.com>
2018-05-28 17:35:18 +0800

    ddd

--- vueExercise/vue-test/src/router/index.js
+++ vueExercise/vue-test/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import bargraph from '@/views/bargraph/index'
+import vueset1 from '@/views/vue-set1/index'
+import vueset2 from '@/views/vue-set2/index'
 
 Vue.use(Router)
 
@@ -11,6 +13,11 @@ export default new Router({
       path: '/bargraph',
       name: 'bargraph',
       component: bargraph
+    },
+    {
+      path: '/vueset2',
+      name: 'vueset2',
+      component: vueset2
     }
   ]
 })
---
+++ vueExercise/vue-test/src/views/vue-set2/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="vue-set">
+      <ul>
+        <li v-for="(item,index) in arrInfo">{{item.name}}--{{item.age}}</li>
+      </ul>
+    </div>
+</template>
+<script>
+export default {
+  name: 'vueset',
+  data(){
+    return {
+      arrInfo:[
+        {'name':'zuobaiquan','age':22},
+        {'name':'zhangsan','age':20}
+      ]
+    }
+  },
+  // created(){
+  //   this.arrInfo[0]={'name':'zuobaiquan01','age':22}
+  // },
+  mounted(){
+    this.arrInfo[0]={'name':'zuobaiquan02','age':22}
+  },
+  methods:{
+    handleClick(index){
+      // this.arrInfo[index].name='zuobaiquan01'
+    }
+  }
+}
+</script>
+<style media="screen" lang="scss">
+ul{
+  li{
+    font-size: 16px;
+    line-height: 40px;
+  }
+}
+</style>
