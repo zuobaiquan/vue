269694356c1f48f9382d57669106cf96fa319e23
zuobaiquan <zuobaiquan@qq.com>
2020-12-24 22:22:47 +0800

    update

--- vueExercise/vue-test/src/router/index.js
+++ vueExercise/vue-test/src/router/index.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 import bargraph from '@/views/bargraph/index'
 import vueset1 from '@/views/vue-set/index01'
 import vueset2 from '@/views/vue-set/index02'
+import swiper from '@/views/swiper/index'
 
 Vue.use(Router)
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/vueset1',
       name: 'vueset1',
       component: vueset1
+    },
+    {
+      path: '/swiper',
+      name: 'swiper',
+      component: swiper
     }
   ]
 })
