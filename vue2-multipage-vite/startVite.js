926efc5145a1339fd4e46396632cd729ce75ce24
zuobaiquan <zuobaiquan@qq.com>
2021-10-12 14:37:51 +0800

    fix

--- vue2-multipage-vite/startVite.js
+++ vue2-multipage-vite/startVite.js
@@ -1,4 +1,4 @@
-const { exec } = require('child_process')
+const { exec } = require('child_process');
 exec("node -v", (error, data, stderr) => {
   if (error) {
     console.log(`error: ${error.message}`);
@@ -24,6 +24,8 @@ exec("node -v", (error, data, stderr) => {
       })
       subprocess.stdout.on('data', (data) => {
         console.log(`${data}`)
+      })
+      subprocess.on('close', (code) => {
       })
     } else {
       console.log('当前node版本过低，Vite 需要 Node.js 版本 >= 12.0.0')
