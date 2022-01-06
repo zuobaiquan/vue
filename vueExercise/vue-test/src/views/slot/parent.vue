43255b5e27e8cfd29ad8159874beb33a91f91b70
zuobaiquan <zuobaiquan@qq.com>
2022-01-06 21:50:51 +0800

    fix

--- vueExercise/vue-test/src/views/slot/parent.vue
+++ vueExercise/vue-test/src/views/slot/parent.vue
@@ -1,21 +1,18 @@
 <template>
   <div>
       <h3>这是父组件</h3>
-      <son>实践slot
-        <template slot="myslot">
-          <div>实践具名slot</div>
-       </template>
+      <son>slot学习
 
-       <template slot="myslot2" slot-scope="scope">
-        <ul>
-          <li v-for="item in scope.data">{{item.name}} -- {{item.age}}</li>
-        </ul>
-      </template>
+        <template slot="slot1" slot-scope="scope">
+          <div>slot 具名插槽</div>
+          <ul>
+            <li v-for="item in scope.data" :key="item.name">{{item.name}} -- {{item.age}}</li>
+          </ul>
+        </template>
       </son>
   </div>
 </template>
 <script>
-// https://www.cnblogs.com/passkey/p/14291370.html#
 import son from './son.vue'
 export default {
   name: 'SlotName',
@@ -35,3 +32,15 @@ export default {
 <style media="screen" lang="scss">
 
 </style>
+
+<!-- <son>slot学习
+        <template slot="myslot">
+          <div>slot学习 具名slot</div>
+       </template>
+
+       <template slot="myslot2" slot-scope="scope">
+        <ul>
+          <li v-for="item in scope.data" :key="item.name">{{item.name}} -- {{item.age}}</li>
+        </ul>
+      </template>
+      </son> -->
--- vueExercise/vue-test/src/views/slot/son.vue
+++ vueExercise/vue-test/src/views/slot/son.vue
@@ -2,24 +2,8 @@
   <div>
       <h4>这是子组件</h4>
       <input type="text" placeholder="请输入">
-      <!-- 
-        子组件中的实践slot是显示不出来的。如果需要让他显示出来，这时我们就可以用到插槽slot
-
-        这里面slot是没有名称的，这是插槽中的默认插槽。
-       -->
-       <slot></slot>
-
-       
-       <slot name="myslot"></slot>
-
-       <!-- slot-scope是作用域插槽 -->
-
-       <slot name="myslot2" :data='list'></slot>
-
-       <!-- 首先，在子组件中的插槽上有一句data="list"，而在父组件中也有slot-scope="scope"，slot-scope就是取data的值，slot-scope的值是自定义的，我们可以取任何名称，但是data的值传过来时是以对象形式传输的，所以在这scope.data才是list的值。
-
-       这样我们就可以在父组件中取到子组件的值，并且加以应用了。 -->
-
+      <slot></slot>
+      <slot name="slot1" :data="list"></slot>
   </div>
 </template>
 <script>
@@ -42,3 +26,21 @@ export default {
 <style media="screen" lang="scss">
 
 </style>
+
+<!-- 
+        子组件中的实践slot是显示不出来的。如果需要让他显示出来，这时我们就可以用到插槽slot
+
+        这里面slot是没有名称的，这是插槽中的默认插槽。
+       -->
+       <slot></slot>
+
+       
+       <slot name="myslot"></slot>
+
+       <!-- slot-scope是作用域插槽 -->
+
+       <slot name="myslot2" :data='list'></slot>
+
+       <!-- 首先，在子组件中的插槽上有一句data="list"，而在父组件中也有slot-scope="scope"，slot-scope就是取data的值，slot-scope的值是自定义的，我们可以取任何名称，但是data的值传过来时是以对象形式传输的，所以在这scope.data才是list的值。
+
+       这样我们就可以在父组件中取到子组件的值，并且加以应用了。 -->
